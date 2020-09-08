fee1f5fb99e6fec956747f33c6345e619faeaed9
Ubuntu <ubuntu@ip-172-31-36-77.ap-southeast-1.compute.internal>
2020-09-08 12:16:21 +0000

    inital commit,added content type

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8b41c237c142726e45d646e3cd6d5c7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '30e4b41c-d006-4654-a7a9-18c1628dc2f7'
+};
