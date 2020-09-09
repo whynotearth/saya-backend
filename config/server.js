be03a2d39913ea872e42ed91a162c57d8e37e8d9
giteshk11 <writemail2gitesh@gmail.com>
2020-09-09 21:30:07 +0530

    changed url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("", "http://localhost:1337"),
+  url: env("", "http://13.212.85.120:1337"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "c8b41c237c142726e45d646e3cd6d5c7"),
