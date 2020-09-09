35ed8de8f7a072fc0ec1b6064ffd2ffbf5eaf232
giteshk11 <writemail2gitesh@gmail.com>
2020-09-09 21:26:13 +0530

    added url prop in config/sever.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("", "http://localhost:1337"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c8b41c237c142726e45d646e3cd6d5c7'),
+      secret: env("ADMIN_JWT_SECRET", "c8b41c237c142726e45d646e3cd6d5c7"),
     },
   },
 });
