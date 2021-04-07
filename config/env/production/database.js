fd935e7887ba8b7f5fb687cae627228f806d2c07
Gitesh <writemail2gitesh@gmail.com>
2021-04-07 12:55:08 +0530

    upgraded to v3.5.4

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,25 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {},
+      options: {
+        ssl: true,
+      },
     },
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL", ""),
+  url: env("URL", "http://localhost:1337"),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET", "9c8fab1fbbacf946ee8896195f67e7fc"),
+      secret: env("ADMIN_JWT_SECRET", "5415af28a2b79f3cd8db55bb7ba475d9"),
     },
   },
 });
